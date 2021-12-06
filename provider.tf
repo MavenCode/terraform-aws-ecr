c98b3fc88dca41b6e8e7548fe0eee16a03ae18ca
en-mac <ian.macdonald@mavencode.com>
2021-12-06 16:47:50 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-}
-
 provider "aws" {
   region     = var.region
   access_key = var.access_key
